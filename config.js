b0073a136aad84d5381fc7a4dd47681ece5c8d69
kingjani12 <abdullhakhanjani13@gmail.com>
2024-12-24 12:28:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ DEBUG =
 module.exports = {
   VERSION: "v1.2.8",
   SESSION:
-    process.env.ASENA_SESSION === undefined ? "" : process.env.ASENA_SESSION,
+    process.env.ASENA_SESSION === undefined ? "levanter_242def8b0d67064f728299a6ff3c54071b" : process.env.ASENA_SESSION,
   EXT: process.env.EXT === undefined ? undefined : process.env.EXT,
   LANG:
     process.env.LANGUAGE === undefined
